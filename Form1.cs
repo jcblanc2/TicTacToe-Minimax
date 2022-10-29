91a369494a010e07afa2f08d99ff54899fc1731f
jcblanc2 <jclaytonblanc@gmail.com>
2022-10-28 22:00:43 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace devoir3
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Name = "00";
+            Logic.checkAndDisplay(btn);
+            Logic.verify(btn);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Name = "01";
+            Logic.checkAndDisplay(btn);
+            Logic.verify(btn);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Name = "02";
+            Logic.checkAndDisplay(btn);
+            Logic.verify(btn);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Name = "10";
+            Logic.checkAndDisplay(btn);
+            Logic.verify(btn);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Name = "11";
+            Logic.checkAndDisplay(btn);
+            Logic.verify(btn);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Name = "12";
+            Logic.checkAndDisplay(btn);
+            Logic.verify(btn);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Name = "20";
+            Logic.checkAndDisplay(btn);
+            Logic.verify(btn);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Name = "21";
+            Logic.checkAndDisplay(btn);
+            Logic.verify(btn);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Name = "22";
+            Logic.checkAndDisplay(btn);
+            Logic.verify(btn);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            button0.Text = String.Empty;
+            button1.Text = String.Empty;
+            button2.Text = String.Empty;
+            button3.Text = String.Empty;
+            button4.Text = String.Empty;
+            button5.Text = String.Empty;
+            button6.Text = String.Empty;
+            button7.Text = String.Empty;
+            button8.Text = String.Empty;
+
+            Logic.clear();
+        }
+    }
+}
---
+++ Logic.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace devoir3
+{
+    static class Logic
+    {
+        static int count = 0;
+        static bool finish = false;
+        static String[,] board = new String[3,3];
+        public static void checkAndDisplay(Button btn)
+        {
+            ++count;
+            if(btn.Text == String.Empty)
+            {
+                if (!finish)
+                {
+                    if(count % 2 == 0)
+                    {
+                        btn.Text = "X";
+                        btn.ForeColor = System.Drawing.Color.Blue;
+                    }
+                    else
+                    {
+                        btn.Text = "O";
+                        btn.ForeColor = System.Drawing.Color.ForestGreen;
+                    }
+                }
+            }
+        }
+
+        public static void verify(Button btn)
+        {
+            if (!finish)
+            {
+                MiniMax.Move bestMove = MiniMax.findBestMove(board);
+
+                int x = (int)Char.GetNumericValue(btn.Name.ElementAt(0));
+                int y = (int)Char.GetNumericValue(btn.Name.ElementAt(1));
+
+                board[x, y] = btn.Text.ToString();
+
+                if (board[0, 0] != null && board[0, 0].Equals(board[1, 1]) && board[1, 1].Equals(board[2, 2]))
+                {
+                    diaplayMessage(board[0, 0]);
+                }
+
+
+                if (board[0, 2] != null && board[0, 2].Equals(board[1, 1]) && board[1, 1].Equals(board[2, 0]))
+                {
+                    diaplayMessage(board[0, 2]);
+                }
+
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (board[i, 0] != null && board[i, 0].Equals(board[i, 1]) && board[i, 1].Equals(board[i, 2]))
+                    {
+                        diaplayMessage(board[i, 0]);
+                    }
+
+                    if (board[0, i]  != null && board[0, i].Equals(board[1, i]) && board[1, i].Equals(board[2, i]))
+                    {
+                        diaplayMessage(board[0, i]);
+                    }
+                }
+
+
+                if (count == 9 && finish != true)
+                {
+                    MessageBox.Show("Draw", "Tic Tac Toe");
+                    finish = true;
+                }
+            }
+        }
+
+
+        private static void diaplayMessage(String winer)
+        {
+            if (!finish)
+            {
+                MessageBox.Show(winer + "   Won", "Tic Tac Toe");
+                finish = true;
+                return;
+            }
+            
+        }
+
+        public static void clear()
+        {
+            finish = false;
+            count = 0;
+            board = new String[3, 3];
+        }
+    }
+}
